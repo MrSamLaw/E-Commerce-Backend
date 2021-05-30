0fefc8f98b7e74aac3641b4c588cb3b16d5ec1bb
Mr Sam Law <sam@chongland.com>
2021-05-30 17:27:12 +1000

    Model Associations

--- models/index.js
+++ models/index.js
@@ -5,12 +5,33 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id'
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'SET NULL',
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  foreignKey: 'product_id',
+})
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  foreignKey: 'tag_id',
+});
 
 module.exports = {
   Product,
