fcb01de4037d3b6bad972bd2ae6dbf2b9435aba3
Mr Sam Law <sam@chongland.com>
2021-06-01 11:08:02 +1000

    testing routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -72,7 +72,7 @@ router.delete('/:id', async (req, res) => {
       res.status(404).json({ message: 'No category found with this id!' });
       return;
     }
-    res.status(200).json(categoryData);
+    res.status(200).json(categoryData.category_name);
   } catch (err) {
     res.status(500).json(err);
   }
