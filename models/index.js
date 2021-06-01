e43f68d0c120e71cf4f6dfdb1124b39574dbe6c6
Mr Sam Law <sam@chongland.com>
2021-06-01 11:43:23 +1000

    Delete Category error

--- models/index.js
+++ models/index.js
@@ -7,13 +7,13 @@ const ProductTag = require('./ProductTag');
 // Products belongsTo Category
 Product.belongsTo(Category, {
   foreignKey: 'category_id',
-  // onDelete: 'CASCADE',
+  onDelete: 'CASCADE',
 });
 
 // Categories have many Products
 Category.hasMany(Product, {
   foreignKey: 'category_id',
-  onDelete: 'SET NULL',
+  onDelete: 'CASCADE',
 });
 
 // Products belongToMany Tags (through ProductTag)
